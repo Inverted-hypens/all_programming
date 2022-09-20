53ef7a9ef683c28d3522f4529a0f4db2ff5634b0
Bestman Sene <sjsoulute@gmail.com>
2022-09-19 21:43:37 -0700

    Fixed task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,20 +8,19 @@
  */
 int main(void)
 {
-	int pass[100];
-	int sum;
-
-	sum = 0;
+	int pass, int sum, int n;
 
 	srand(time(NULL));
+	sum = 0;
 
-	while (sum <= 100)
+	while (sum <= 2645)
 	{
-		pass = (rand() % 78);
-		sum += pass;
-		printf("%c", pass);
+		pass = (rand() % 128);
+		sum += (pass + '0');
+		printf("%c", (pass + '0'));
 	}
-	printf("%c", 2772 - sum);
+	n = 2772 - sum
+	printf("%c", n + '0');
 
 	return (0);
 }
