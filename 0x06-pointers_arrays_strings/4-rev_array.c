3155b58af0c22e9ae330c7574e71d6f5c00e0bd7
Bestman Sene <sjsoulute@gmail.com>
2022-09-23 06:36:32 +0000

    Added all main tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - revereses an array of integers
+ * @a: pointer to array.
+ * @n: number of elements of an array.
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, counter;
+
+	n = n - 1;
+	counter = 0;
+
+	while (counter <= n)
+	{
+		temp = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temp;
+	}
+}
